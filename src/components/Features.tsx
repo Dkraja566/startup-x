6c881ae96c175361f16087e76327eb8b3da8a966
Dhirendra Singh <127616069+Dkraja566@users.noreply.github.com>
2025-02-27 12:01:38 +0530

    Update Features.tsx

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -150,7 +150,7 @@ export const Features = () => {
       <div className="container px-4 md:px-6">
         <div className="text-center space-y-4 mb-8 md:mb-16">
           <motion.div
-            initial={{ opacity: 0, y: 20 }}
+            initial={{ opacity: 0, y: 15 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
             viewport={{ once: true }}
